64cc6c194e8c3266124f0aa36496068480fe1c80
Camyul <vankata.lt@outlook.com>
2017-09-18 15:10:29 +0300

    First ASP.NET MVC5 app Initial

---
+++ ASP.NET MVC/AspNetMvcFirstApp/AspNetMvcFirstApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMvcFirstApp.Startup))]
+namespace AspNetMvcFirstApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
